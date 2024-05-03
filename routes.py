158761c4c44c7305f640906739f4e98eeba823fc
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-03 15:13:49 +1200

    Started a menu page but a few errors still, and trying to add a nav bar but still not working :(

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import sqlite3
 
 app = Flask(__name__)
 
@@ -8,6 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/menu', methods=['GET'])
+def movie():
+    conn = sqlite3.connect('Database/pizza.db')
+    cur = conn.cursor()
+    pizza = cur.fetchone()
+    cur.execute('SELECT * FROM Pizza WHERE id=?' ("?"))
+    return render_template('menu.html', pizza=pizza)
 
 
 if __name__ == "__main__":
