b0335ce55e44ccacaed397f29bef9eb2e46414f9
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-07 12:37:44 +1200

    fixed small errors and connected data to my site

--- routes.py
+++ routes.py
@@ -4,19 +4,41 @@ import sqlite3
 app = Flask(__name__)
 
 
-# Route for the home page
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template("home.html", title="Home")
 
 
-@app.route('/menu', methods=['GET'])
-def movie():
+@app.route('/pizza/<int:id>')
+def pizza(id):
     conn = sqlite3.connect('Database/pizza.db')
     cur = conn.cursor()
+    cur.execute('SELECT * FROM Pizza WHERE id=?', (id,))
     pizza = cur.fetchone()
-    cur.execute('SELECT * FROM Pizza WHERE id=?' ("?"))
-    return render_template('menu.html', pizza=pizza)
+    cur.execute('SELECT * FROM Base WHERE id=?', (pizza[2],))
+    base = cur.fetchone()
+    cur.execute('SELECT * FROM Pizza WHERE id=?', (pizza[1],))
+    topping = cur.fetchone()
+    return render_template('pizza.html', pizza=pizza, base=base, topping=topping)
+
+
+@app.route('/offers')
+def offers():
+    return render_template("offers.html", title="Contact")
+
+
+@app.route('/stores')
+def stores():
+    return render_template("stores.html", title="About")
+
+
+@app.route('/menu')
+def menu():
+    conn = sqlite3.connect('Database/pizza.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Pizza')
+    results = cur.fetchall()
+    return render_template('menu.html', results=results)
 
 
 if __name__ == "__main__":
